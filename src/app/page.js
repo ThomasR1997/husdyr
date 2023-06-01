cb0863842eaf5dd745233bf89f1beb554fc3b28f
hftjutyir <thomas.kodehode@gmail.com>
2023-06-01 14:28:17 +0200

    added images & minor styling to navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,48 @@
 import Seo from "@/components/seo";
+import Image from "next/image";
+import Link from "next/link";
+
+import dogs from "../images/dogs.jpg";
+import cats from "../images/cats.jpg";
+import rabbits from "../images/rabbits.jpg";
 
 export default function Home() {
   return (
     <div>
       <Seo pageTitle="Husdyr" pageDescription="Hjemmeside" />
       <h1>Husdyr</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+
+      <div>
+        <div>
+          <h2>Hunder</h2>
+          <Link href="/hund">
+            <Image src={dogs} width={300} height={300} alt="hunder" />
+          </Link>
+        </div>
+
+        <div>
+          <h2>Katter</h2>
+          <Link href="/katt">
+            <Image src={cats} width={300} height={300} alt="katter" />
+          </Link>
+        </div>
+
+        <div>
+          <h2>Kaniner</h2>
+          <Link href="/kanin">
+            <Image src={rabbits} width={300} height={300} alt="kaniner" />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,15 +2,15 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div>
-      <div>
-        <Link href="/">Logo</Link>
-        <h1>Husdyr</h1>
+    <div className="flex items-center justify-between p-5 border ">
+      <div className="flex space-x-20">
+        <h1>Logo</h1>
+        <Link href="/">Husdyr</Link>
       </div>
-      <div>
-        <Link href="/hund">Hund</Link>
-        <Link href="/katt">Katt</Link>
-        <Link href="/kanin">Kanin</Link>
+      <div className="space-x-20 border">
+        <Link href="/hund">Hunder</Link>
+        <Link href="/katt">Katter</Link>
+        <Link href="/kanin">Kaniner</Link>
       </div>
     </div>
   );
