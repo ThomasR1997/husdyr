acd4dbc2ab451ca3ec98429f4587e79072d3b081
hftjutyir <thomas.kodehode@gmail.com>
2023-06-05 09:14:21 +0200

    fixed main page layout

--- src/app/page.js
+++ src/app/page.js
@@ -8,35 +8,38 @@ import rabbits from "../images/rabbits.jpg";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center border">
       <Seo pageTitle="Husdyr" pageDescription="Hjemmeside" />
-      <h1>Husdyr</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+      <h1 className="mt-10">Husdyr</h1>
 
-      <div>
-        <div>
+      <div className="w-1/2 my-10">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+
+      <div className="flex space-x-12 border">
+        <div className="border">
           <h2>Hunder</h2>
           <Link href="/hund">
             <Image src={dogs} width={300} height={300} alt="hunder" />
           </Link>
         </div>
 
-        <div>
+        <div className="border">
           <h2>Katter</h2>
           <Link href="/katt">
             <Image src={cats} width={300} height={300} alt="katter" />
           </Link>
         </div>
 
-        <div>
+        <div className="border">
           <h2>Kaniner</h2>
           <Link href="/kanin">
             <Image src={rabbits} width={300} height={300} alt="kaniner" />
