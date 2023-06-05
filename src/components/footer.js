04b96545302694f973cd9887601965ee00b33ecb
hftjutyir <thomas.kodehode@gmail.com>
2023-06-05 09:33:07 +0200

    added footer

---
+++ src/components/footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="absolute bottom-0 left-0 right-0 flex items-center justify-center h-20 border">
+      <p>Elementum integer enim neque volutpat ac tincidunt vitae.</p>
+    </div>
+  );
+}
